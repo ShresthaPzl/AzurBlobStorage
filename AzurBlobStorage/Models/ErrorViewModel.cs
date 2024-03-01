f6002c140c9129ee4e254a7ba0ae92d3fb6f9187
Pzl <Pzlshrestha7@gmail.com>
2024-03-01 08:46:01 +0545

    Initial commit

---
+++ AzurBlobStorage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzurBlobStorage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
